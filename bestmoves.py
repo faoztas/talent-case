cf2c942cb0983f830bc64224f183af62bb23054c
frknnlzts <trpc61@gmail.com>
2019-07-11 00:43:20 +0300

    hello world!

---
+++ bestmoves.py
@@ -0,0 +1,9 @@
+checkers = {1: 3, 6: 1, 10: 2, 12: 1, 13: 1}
+important_doors = [5, 6, 7, 8, 17, 18, 19, 20]
+
+def find_moves(checkers, dice1, dice2)
+    ...
+    ...
+    ...
+
+print(find_moves(checkers, 6, 1))
