4baf24dcd4d1741ce0788728a342497cce4f9580
frknnlzts <trpc61@gmail.com>
2019-07-11 01:42:02 +0300

    add moves

--- bestmoves.py
+++ bestmoves.py
@@ -1,9 +1,18 @@
 checkers = {1: 3, 6: 1, 10: 2, 12: 1, 13: 1}
 important_doors = [5, 6, 7, 8, 17, 18, 19, 20]
+neoc = checkers.copy()
 
 def find_moves(checkers, dice1, dice2)
-    ...
-    ...
-    ...
+    for stamp1 in checkers.keys():
+        point1 = stamp1 + dice1
+
+        for stamp2 in checkers.keys():
+            point2 = stamp2 + dice2
+
+        if point1 in neoc:
+            neoc[point1] += 1
+        else:
+            neoc[point1]=1
+
 
 print(find_moves(checkers, 6, 1))
