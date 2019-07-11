d7bf271df3a906d5672e06c1c9e11e4603921215
frknnlzts <trpc61@gmail.com>
2019-07-12 01:01:37 +0300

    Moves Completed

--- bestmoves.py
+++ bestmoves.py
@@ -1,18 +1,31 @@
 checkers = {1: 3, 6: 1, 10: 2, 12: 1, 13: 1}
-important_doors = [5, 6, 7, 8, 17, 18, 19, 20]
-neoc = checkers.copy()
+importantDoors = [5, 6, 7, 8, 17, 18, 19, 20]
+neoCheckers = checkers.copy()
+beforeScore = 0
+afterScore = 0
 
 def find_moves(checkers, dice1, dice2)
     for stamp1 in checkers.keys():
-        point1 = stamp1 + dice1
+        point1 = stamp1 + dice1             #Taş ve zar ile pozisyon hesabı
 
-        for stamp2 in checkers.keys():
-            point2 = stamp2 + dice2
+        if point1 < 24:                     #Tavla tahta sınırları kontrolü
+            for stamp2 in checkers.keys():
+                point2 = stamp2 + dice2     #Taş ve zar ile pozisyon hesabı
 
-        if point1 in neoc:
-            neoc[point1] += 1
-        else:
-            neoc[point1]=1
+                if point2 < 24:             #Tavla tahta sınırları kontrolü
+
+                    if point1 in neoCheckers:
+                        neoCheckers[point1] += 1   #kapı alma
+                    else:
+                        neoCheckers[point1] = 1
+
+                    if point2 in neoCheckers:
+                        neoCheckers[point2] += 1   #kapı alma
+                    else:
+                        neoCheckers[point2] = 1
+
+                    neoCheckers[stamp1] -= 1       #hareket tamamlama
+                    neoCheckers[stamp2] -= 1
 
 
 print(find_moves(checkers, 6, 1))
