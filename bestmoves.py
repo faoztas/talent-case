0b82edaccc8f1e44cdc1d679f656450218421f4f
frknnlzts <trpc61@gmail.com>
2019-07-12 01:54:59 +0300

    Score Calculating Completed

--- bestmoves.py
+++ bestmoves.py
@@ -1,10 +1,16 @@
 checkers = {1: 3, 6: 1, 10: 2, 12: 1, 13: 1}
-importantDoors = [5, 6, 7, 8, 17, 18, 19, 20]
-neoCheckers = checkers.copy()
-beforeScore = 0
-afterScore = 0
 
-def find_moves(checkers, dice1, dice2)
+def find_moves(checkers, dice1, dice2):
+
+##################################################
+    importantDoors = [5, 6, 7, 8, 17, 18, 19, 20]
+    score = []
+    bestMoves = []
+    copyCheckers = checkers.copy()
+    beforeScore = 0
+    afterScore = 0
+##################################################
+
     for stamp1 in checkers.keys():
         point1 = stamp1 + dice1             #Taş ve zar ile pozisyon hesabı
 
@@ -14,18 +20,44 @@ def find_moves(checkers, dice1, dice2)
 
                 if point2 < 24:             #Tavla tahta sınırları kontrolü
 
-                    if point1 in neoCheckers:
-                        neoCheckers[point1] += 1   #kapı alma
+                    if point1 in copyCheckers:
+                        copyCheckers[point1] += 1   #kapı alma
                     else:
-                        neoCheckers[point1] = 1
+                        copyCheckers[point1] = 1
 
-                    if point2 in neoCheckers:
-                        neoCheckers[point2] += 1   #kapı alma
+                    if point2 in copyCheckers:
+                        copyCheckers[point2] += 1   #kapı alma
                     else:
-                        neoCheckers[point2] = 1
+                        copyCheckers[point2] = 1
 
-                    neoCheckers[stamp1] -= 1       #hareket tamamlama
-                    neoCheckers[stamp2] -= 1
+                    copyCheckers[stamp1] -= 1       #hareket tamamlama
+                    copyCheckers[stamp2] -= 1
+
+                    for i in checkers.keys():       #mevcut puanı hesaplama
+                        if checkers[i] == 1:
+                            beforeScore -= 1
+                        if checkers[i] == 2:
+                            if i in importantDoors:
+                                beforeScore += 2
+                            else:
+                                beforeScore +=1
+                        if checkers[i] > 2:
+                            score.append(i)
+
+                    for i in copyCheckers.keys():   #hamle sonrası puan hesaplama
+                        if copyCheckers[i] == 1:
+                            afterScore -= 1
+                        if copyCheckers[i] == 2 & i not in score:
+                            if i in importantDoors:
+                                afterScore += 2
+                            else:
+                                afterScore += 1
+
+
+                    if afterScore - beforeScore > 0:
+                        bestMoves.append((stamp1,point1),(stamp2,point2),afterScore-beforeScore)
+    return bestMoves
+
 
 
 print(find_moves(checkers, 6, 1))
